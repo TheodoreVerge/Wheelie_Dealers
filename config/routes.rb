f3a7f8728563d2f592195dd05405a18179dc4afa
Theodore Verge <theodore.verge@gmail.com>
2020-02-24 17:28:22 +0000

    transaction route udpdate

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :bikes, only: [:index, :show, :create, :new, :destroy]
+  resources :bikes, only: [:index, :show, :create, :new, :destroy] do
+    resources :transactions, only: [:create]
+  end
   resources :users, only: [:show] do
     resources :reviews, only: [:index, :create]
   end
