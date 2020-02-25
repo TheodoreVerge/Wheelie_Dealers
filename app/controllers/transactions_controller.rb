d9fb47767d2bd6178a100eb6d07b1eaeabb4a8c4
Alexander Stewart <ac_stewart@icloud.com>
2020-02-25 15:56:51 +0000

    transaction conroller, model, and view functional

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,20 @@
 class TransactionsController < ApplicationController
+  def show
+    @transaction = Transaction.find(params[:id])
+  end
+
+  def new
+    @transaction = Transaction.new
+  end
+
+  def create
+    @bike = Bike.find(params[:bike_id])
+    @user = current_user
+    @transaction = Transaction.new(user_id: @user.id, bike_id: @bike.id)
+    if @transaction.save
+      redirect_to transaction_path(@transaction)
+    else
+      redirect_to bike_path(@bike)
+    end
+  end
 end
