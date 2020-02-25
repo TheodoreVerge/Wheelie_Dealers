b0e06638b56fe244b9450a2921128f30e4ecebd2
Alexander Stewart <ac_stewart@icloud.com>
2020-02-25 17:32:07 +0000

    Add dotenv - Protect my secret data in .env file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # private
+
+  # def configure_permitted_paramaters
+  #   devise_paramater_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :postcode])
+  # end
 end
