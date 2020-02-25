94ed36c448135b3cc96d90ab469e17e49fd3cbf6
Theodore Verge <theodore.verge@gmail.com>
2020-02-25 14:06:08 +0000

    finished the bikes model and views

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,30 +1,37 @@
 class BikesController < ApplicationController
 
   def index
-  @bikes = Bike.all
+    @bikes = Bike.all
   end
 
   def show
-  @bike = Bike.find(params[:id])
+    @bike = Bike.find(params[:id])
   end
 
   def new
-  @bike = Bike.new
+    @bike = Bike.new
   end
 
   def create
-  @bike = Bike.create(bike_params)
-  raise
-  if @bike.save
-    redirect_to bikes_path
-  else
-    render :new
+    @user = current_user.id
+    @bike = Bike.create(bike_params)
+    @bike.user_id = @user
+      if @bike.save
+        redirect_to bike_path(@bike)
+      else
+        render :new
+      end
   end
+
+  def destroy
+    @bike = Bike.find(params[:id])
+    @bike.destroy
+    redirect_to bikes_path
   end
 
    private
 
   def bike_params
-    params.require(:bike).permit(:id, :price, :description, :brand)
+    params.require(:bike).permit(:title, :price, :description, :brand)
   end
 end
