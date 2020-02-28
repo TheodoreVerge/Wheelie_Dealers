2891b612fb7aa44feae7d74f974a84d826d8fcfc
Zubair Malek <zubairmalek95@gmail.com>
2020-02-28 12:04:18 +0000

    Show page functional with included map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -3,7 +3,7 @@ import mapboxgl from 'mapbox-gl';
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, speed: 10 });
+  // map.fitBounds(bounds, { padding: 70, maxZoom: 15, speed: 10 });
 };
 
 const initMapbox = () => {
@@ -16,7 +16,7 @@ const initMapbox = () => {
       style: 'mapbox://styles/stewartac/ck73wbflb215a1jmxecil0br9'
       });
     const markers = JSON.parse(mapElement.dataset.markers);
-      markers.forEach((marker) => {
+    markers.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
@@ -25,6 +25,6 @@ const initMapbox = () => {
     });
     fitMapToMarkers(map, markers);
   }
-  };
+};
 
 export { initMapbox };
